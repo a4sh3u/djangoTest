84c1299dc213c907416cc3679fe2adca158bd069
Ashutosh Singh <ashutosh.singh@ubitricity.com>
2018-04-01 02:57:47 +0200

    no message

--- deploy/aws_provision.sh
+++ deploy/aws_provision.sh
@@ -1,6 +1,11 @@
-aws cloudformation create-stack --stack-name myteststack --template-body file:///root/djangoTest/deploy/cf.template --parameters ParameterKey=KeyName,ParameterValue=djangoKey
+#!/bin/bash
+aws cloudformation create-stack --stack-name myteststack --template-body file://deploy/cf.template --parameters ParameterKey=KeyName,ParameterValue=djangoKey
 
-ec2_ip=$(aws cloudformation describe-stacks | grep "OutputValue" | awk -F '"' '{print $4}' | grep -v i)
+while [ -z "${ec2_ip}" ]; do
+  sleep 5;
+  ec2_ip=$(aws cloudformation describe-stacks | grep "OutputValue" | awk -F '"' '{print $4}' | grep -v i)
+done
+
 scp -o StrictHostKeyChecking=no -i "djangoKey.pem" ./deploy/instance_provision.sh ubuntu@${ec2_ip}:/home/ubuntu
 ssh -o StrictHostKeyChecking=no -i "djangoKey.pem" ubuntu@${ec2_ip} "chmod +x /home/ubuntu/instance_provision.sh; /home/ubuntu/instance_provision.sh"
 scp -o StrictHostKeyChecking=no -i "djangoKey.pem" ./deploy/app_deploy.sh ubuntu@${ec2_ip}:/home/ubuntu
