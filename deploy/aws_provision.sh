cde84d9a11bacc5ef6eab950c05a7d25f91495dd
Ashutosh Singh <ashutosh.singh@ubitricity.com>
2018-04-01 02:38:59 +0200

    no message

--- deploy/aws_provision.sh
+++ deploy/aws_provision.sh
@@ -2,6 +2,9 @@ aws cloudformation create-stack --stack-name myteststack --template-body file://
 
 ec2_ip=$(aws cloudformation describe-stacks | grep "OutputValue" | awk -F '"' '{print $4}' | grep -v i)
 scp -o StrictHostKeyChecking=no -i "djangoKey.pem" ./deploy/instance_provision.sh ubuntu@${ec2_ip}:/home/ubuntu
-ssh -o StrictHostKeyChecking=no -i "djangoKey.pem" ubuntu@${ec2_ip} "/home/ubuntu/instance_provision.sh"
+ssh -o StrictHostKeyChecking=no -i "djangoKey.pem" ubuntu@${ec2_ip} "chmod +x /home/ubuntu/instance_provision.sh; /home/ubuntu/instance_provision.sh"
 scp -o StrictHostKeyChecking=no -i "djangoKey.pem" ./deploy/app_deploy.sh ubuntu@${ec2_ip}:/home/ubuntu
-ssh -o StrictHostKeyChecking=no -i "djangoKey.pem" ubuntu@${ec2_ip} "/home/ubuntu/app_deploy.sh"
+ssh -o StrictHostKeyChecking=no -i "djangoKey.pem" ubuntu@${ec2_ip} "chmod +x /home/ubuntu/app_deploy.sh;/home/ubuntu/app_deploy.sh"
+
+
+#aws cloudformation delete-stack --stack-name "myteststack"
